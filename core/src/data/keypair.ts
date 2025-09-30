302eed02eb7680cbeb9065734964731dbe90d59b
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-09-30 10:32:54 +0900

    keypair-working

--- core/src/data/keypair.ts
+++ core/src/data/keypair.ts
@@ -2,8 +2,9 @@ import { Signature } from "../types";
 import { base58Decode, base58Encode } from "./base58";
 import { Pubkey } from "./pubkey";
 
-export class Keypair {
-  public readonly pubkey: Pubkey;
+// TODO - naming and naming casing
+export class KeyPair {
+  public readonly pubKey: Pubkey;
   public readonly sign: (message: Uint8Array) => Promise<Signature>;
   public readonly verify: (
     message: Uint8Array,
@@ -11,32 +12,49 @@ export class Keypair {
   ) => Promise<boolean>;
 
   private constructor(
-    pubkey: Pubkey,
+    pubKey: Pubkey,
     sign: (message: Uint8Array) => Promise<Signature>,
     verify: (message: Uint8Array, signature: Signature) => Promise<boolean>,
   ) {
-    this.pubkey = pubkey;
+    this.pubKey = pubKey;
     this.sign = sign;
     this.verify = verify;
   }
 
-  static async generate(): Promise<Keypair> {
+  static async generate(): Promise<KeyPair> {
     if (globalThis.crypto?.subtle !== undefined) {
-      return this.generateWebCrypto();
+      return this.generateWeb();
     }
-    return this.generateNodeCrypto();
+    return this.generateNode();
   }
 
-  static async generateWebCrypto(): Promise<Keypair> {
+  static async fromSecret(secret: Uint8Array): Promise<KeyPair> {
+    if (secret.length != 64) {
+      throw new Error(
+        `KeyPair: Expected a secret of 64 bytes (found ${secret.length})`,
+      );
+    }
+    let keyPair: KeyPair;
+    if (globalThis.crypto?.subtle !== undefined) {
+      keyPair = await this.fromSecretWeb(secret);
+    }
+    keyPair = await this.fromSecretNode(secret);
+    const message = new Uint8Array();
+    if (!(await keyPair.verify(message, await keyPair.sign(message)))) {
+      throw new Error(`KeyPair: Secret public and private key mismatch`);
+    }
+    return keyPair;
+  }
+
+  private static async generateWeb(): Promise<KeyPair> {
     const { publicKey, privateKey } = await crypto.subtle.generateKey(
       { name: "Ed25519" },
       false,
       ["sign", "verify"],
     );
     const spki = await crypto.subtle.exportKey("spki", publicKey);
-    const pkRaw = extractRawEd25519Pub(spki);
-    return new Keypair(
-      base58Encode(pkRaw),
+    return new KeyPair(
+      extractPubkeyFromSpki(new Uint8Array(spki)),
       async (message: Uint8Array) => {
         return base58Encode(
           new Uint8Array(
@@ -59,13 +77,79 @@ export class Keypair {
     );
   }
 
-  static async generateNodeCrypto(): Promise<Keypair> {
+  private static async fromSecretWeb(secret: Uint8Array): Promise<KeyPair> {
+    const privateKey = await crypto.subtle.importKey(
+      "pkcs8",
+      pkcs8FromEd25519(secret.slice(0, 32)) as BufferSource,
+      { name: "Ed25519" },
+      false,
+      ["sign"],
+    );
+    const publicKey = await crypto.subtle.importKey(
+      "spki",
+      spkiFromEd25519(secret.slice(32, 64)) as BufferSource,
+      { name: "Ed25519" },
+      true,
+      ["verify"],
+    );
+    const spki = await crypto.subtle.exportKey("spki", publicKey);
+    // TODO - re-use this part
+    return new KeyPair(
+      extractPubkeyFromSpki(new Uint8Array(spki)),
+      async (message: Uint8Array) => {
+        return base58Encode(
+          new Uint8Array(
+            await crypto.subtle.sign(
+              "Ed25519",
+              privateKey,
+              message as BufferSource,
+            ),
+          ),
+        );
+      },
+      async (message: Uint8Array, signature: Signature) => {
+        return await crypto.subtle.verify(
+          "Ed25519",
+          publicKey,
+          base58Decode(signature) as BufferSource,
+          message as BufferSource,
+        );
+      },
+    );
+  }
+
+  private static async generateNode(): Promise<KeyPair> {
     const crypto = await import("crypto");
     const { publicKey, privateKey } = crypto.generateKeyPairSync("ed25519");
     const spki = publicKey.export({ type: "spki", format: "der" });
-    const pkRaw = extractRawEd25519Pub(spki.buffer as ArrayBuffer);
-    return new Keypair(
-      base58Encode(pkRaw),
+    return new KeyPair(
+      extractPubkeyFromSpki(new Uint8Array(spki)),
+      async (message: Uint8Array) => {
+        return base58Encode(
+          new Uint8Array(crypto.sign(null, message, privateKey)),
+        );
+      },
+      async (message: Uint8Array, signature: Signature) => {
+        return crypto.verify(null, message, publicKey, base58Decode(signature));
+      },
+    );
+  }
+
+  private static async fromSecretNode(secret: Uint8Array): Promise<KeyPair> {
+    const crypto = await import("crypto");
+    const privateKey = crypto.createPrivateKey({
+      key: Buffer.from(pkcs8FromEd25519(secret.slice(0, 32))),
+      format: "der",
+      type: "pkcs8",
+    });
+    const publicKey = crypto.createPublicKey({
+      key: Buffer.from(spkiFromEd25519(secret.slice(32, 64))),
+      format: "der",
+      type: "spki",
+    });
+    const spki = publicKey.export({ type: "spki", format: "der" });
+    return new KeyPair(
+      extractPubkeyFromSpki(new Uint8Array(spki)),
       async (message: Uint8Array) => {
         return base58Encode(
           new Uint8Array(crypto.sign(null, message, privateKey)),
@@ -78,7 +162,48 @@ export class Keypair {
   }
 }
 
-function extractRawEd25519Pub(spkiDer: ArrayBuffer): Uint8Array {
-  const bytes = new Uint8Array(spkiDer);
-  return bytes.slice(-32);
+function extractPubkeyFromSpki(spkiDer: Uint8Array): string {
+  return base58Encode(spkiDer.slice(-32));
+}
+
+function pkcs8FromEd25519(blob32: Uint8Array): Uint8Array {
+  let index = 0;
+  const pkcs8 = new Uint8Array(48);
+  pkcs8[index++] = 0x30;
+  pkcs8[index++] = 0x2e; // SEQ len=46
+  pkcs8[index++] = 0x02;
+  pkcs8[index++] = 0x01;
+  pkcs8[index++] = 0x00; // INT 0
+  pkcs8[index++] = 0x30;
+  pkcs8[index++] = 0x05; // SEQ len=5
+  pkcs8[index++] = 0x06;
+  pkcs8[index++] = 0x03;
+  pkcs8[index++] = 0x2b;
+  pkcs8[index++] = 0x65;
+  pkcs8[index++] = 0x70; // OID 1.3.101.112 (Ed25519)
+  pkcs8[index++] = 0x04;
+  pkcs8[index++] = 0x22; // OCTET STRING len=34
+  pkcs8[index++] = 0x04;
+  pkcs8[index++] = 0x20; //   inner OCTET STRING len=32
+  pkcs8.set(blob32, 16);
+  return pkcs8;
+}
+
+function spkiFromEd25519(blob32: Uint8Array): Uint8Array {
+  let index = 0;
+  const spki = new Uint8Array(44);
+  spki[index++] = 0x30;
+  spki[index++] = 0x2a; // SEQUENCE, len 42
+  spki[index++] = 0x30;
+  spki[index++] = 0x05; // SEQUENCE, len 5
+  spki[index++] = 0x06;
+  spki[index++] = 0x03; // OID, len 3
+  spki[index++] = 0x2b;
+  spki[index++] = 0x65;
+  spki[index++] = 0x70; // 1.3.101.112 (Ed25519)
+  spki[index++] = 0x03;
+  spki[index++] = 0x21;
+  spki[index++] = 0x00; // BIT STRING, len 33, 0 unused bits
+  spki.set(blob32, 12);
+  return spki;
 }
---
+++ core/tests/unit.keypair.fromSecret.ts
@@ -0,0 +1,18 @@
+import { it } from "@jest/globals";
+import { Keypair } from "@solana/web3.js";
+import { KeyPair } from "../src/data/keypair";
+
+const secret = new Uint8Array([
+  96, 11, 209, 132, 49, 92, 144, 135, 105, 211, 34, 171, 125, 156, 217, 148, 65,
+  233, 239, 86, 149, 37, 180, 226, 120, 139, 152, 126, 199, 116, 104, 184, 10,
+  85, 215, 5, 230, 110, 192, 255, 29, 27, 96, 27, 203, 56, 119, 189, 226, 99,
+  13, 150, 68, 70, 138, 190, 182, 126, 125, 69, 25, 66, 190, 239,
+]);
+
+it("run", async () => {
+  const referenceKeypair = Keypair.fromSecretKey(secret);
+  const currentKeypair = await KeyPair.fromSecret(secret);
+  expect(currentKeypair.pubKey).toStrictEqual(
+    referenceKeypair.publicKey.toBase58(),
+  );
+});
